6ccd0c0ddbb4a020c203b34f874daf69913b09f6
irboi746 <irboi746@gmail.com>
2023-06-18 17:28:33 +0800

    180623:1728

--- apimap.py
+++ apimap.py
@@ -12,11 +12,11 @@ def printBanner():
 ╚═╝░░╚═╝╚═╝░░░░░╚═╝╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░░░░╯
                                   
 (!) Legal Disclaimer : APIMAP is intended solely for security testing and vulnerability assesment of authorized APIs.
-Users are responsible for obtaining proper authorization and consent before conducting and scans. 
-The tool's creator and contributor assume no liability for any misuse or illegal activities conducted using this software.
+Users are responsible for obtaining proper authorization and consent before conducting and scans. The tool's creator 
+and contributor assume no liability for any misuse or illegal activities conducted using this software.
 
-(~) License : APIMAP is released under the GNU General Public License v3.0. 
-This open-source license grants users the freedom to use, modify, and distibute the software while ensuring that any modifications remain open-source.
+(~) License : APIMAP is released under the GNU General Public License v3.0. This open-source license grants users the 
+freedom to use, modify, and distibute the software while ensuring that any modifications remain open-source.
 	""") 
 
 def main():
